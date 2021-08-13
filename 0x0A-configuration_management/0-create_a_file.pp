86e33886423db7bf3d67f4581ce4ad7a6224c3d6
Miguel22247 <minipachru@gmail.com>
2021-08-13 09:35:04 -0300

    fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
 file { 'holberton'
-    path = '/tmp/holberton',
-    mode = '0744',
-    content = 'I love Puppet',
-    owner = 'www-data',
-    group = 'www-data',
+    path => '/tmp/holberton',
+    mode => '0744',
+    content => 'I love Puppet',
+    owner => 'www-data',
+    group => 'www-data',
 }
