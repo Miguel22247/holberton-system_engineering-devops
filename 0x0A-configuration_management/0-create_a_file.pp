822c9ca96c0d5980bb33036da4d9ec2580d36f53
Miguel22247 <minipachru@gmail.com>
2021-08-13 09:13:51 -0300

    fixed linter?

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,6 @@
 file { '/tmp/holberton'
-	path = /tmp/holberton,
-	owner = 'www-data',
-	group = 'www-data',
-	mode = '0744'
+    path = /tmp/holberton,
+    owner = 'www-data',
+    group = 'www-data',
+    mode = '0744'
 }
