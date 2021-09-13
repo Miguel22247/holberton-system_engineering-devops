09fbca1ea018eb894677350a18203cfdee634ba5
Miguel22247 <minipachru@gmail.com>
2021-09-13 12:56:52 -0300

    add 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 """Script that given employee ID, return information about his list progress"""
+import csv
 import requests
 from sys import argv
-import csv
 
-if __name__ == '__main__':
-    url = "https://jsonplaceholder.typicode.com/"
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
     USER_ID = argv[1]
-    getter = requests.get(url + 'users?id' + USER_ID)
-    file_getter = getter.json()
-    USERNAME = file_getter[0].get('username')
-    request = requests.get(url + 'todos?usersId' + USER_ID)
+    getter = requests.get(url + 'users?id=' + USER_ID)
+    getter_file = getter.json()
+    USERNAME = getter_file[0].get('username')
+
+    request = requests.get(url + 'todos?userId=' + USER_ID)
     request_file = request.json()
     status = []
     titles = []
-
-    for file_content in range(len(request_file)):
-        status.append(request_file[file_content].get('completed'))
-        titles.append(request_file[file_content].get('title'))
-    filename = USER_ID + ".csv"
+    for content in range(len(request_file)):
+        status.append(request_file[content].get('completed'))
+        titles.append(request_file[content].get('title'))
+    filename = USER_ID + '.csv'
     with open(filename, mode='w') as file_:
         task = csv.writer(file_, delimiter=',',
                           quotechar='"', quoting=csv.QUOTE_ALL)
