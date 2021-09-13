0d3ad922621ade4f15bdb664deca29439e44fee0
Miguel22247 <minipachru@gmail.com>
2021-09-13 11:27:09 -0300

    add 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Script that given employee ID, return information about his list progress"""
+import requests
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    url = "https://jsonplaceholder.typicode.com/"
+    USER_ID = argv[1]
+    getter = requests.get(url + 'users?id' + USER_ID)
+    file_getter = getter.json()
+    USERNAME = file_getter[0].get('username')
+    request = requests.get(url + 'todos?usersId' + USER_ID)
+    request_file = request.json()
+    status = []
+    titles = []
+
+    for file_content in range(len(request_file)):
+        status.append(request_file[file_content].get('completed'))
+        titles.append(request_file[file_content].get('title'))
+    filename = USER_ID + ".csv"
+    with open(filename, mode='w') as file_:
+        task = csv.writer(file_, delimiter=',',
+                          quotechar='"', quoting=csv.QUOTE_ALL)
+        for q in range(len(status)):
+            task.writerow([USER_ID, USERNAME, status[q], titles[q]])
