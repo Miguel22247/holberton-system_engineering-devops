c044e5ad0ef8755a488254f59926ddef582d58f0
Miguel22247 <minipachru@gmail.com>
2021-09-28 10:11:56 -0300

    add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Function that queries the reddit API"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Returns the number of subscribers"""
+    url = 'https://www.reddit.com/r' + subreddit + "/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows)'}
+    res_status = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res_status.status_code == 200:
+        return res_status.json()['data'].get('subscribers')
+    else:
+        return 0
