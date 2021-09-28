a0b7595c5e6c4987810b083477b2ad717f167f96
Miguel22247 <minipachru@gmail.com>
2021-09-28 10:29:19 -0300

    add 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""Function that queries the reddit API"""
+import requests
+
+
+def top_ten(subreddit):
+    """Returns the top ten"""
+    url = 'https://www.reddit.com/r' + subreddit + "/hot.json" + '?limit=10'
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows)'}
+    res_status = requests.get(url, headers=headers, allow_redirects=False)
+
+    if res_status.status_code == 200:
+        top_ten = res_status.json()['data']['children']
+        for title in top_ten:
+            print(title)['data']['title']
+    else:
+        print(None)
