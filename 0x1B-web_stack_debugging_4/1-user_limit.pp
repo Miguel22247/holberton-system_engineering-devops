247e970461546e35935d9954cce63b55a5aa2c3c
Miguel Pacheco <minipachru@gmail.com>
2021-10-07 09:12:10 -0300

    fix puppet lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,10 +1,10 @@
-# user holberton
+# User holberton
 exec { 'fixithard':
- command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
- provider => 'shell'
+  command  => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
 
 exec { 'fixitsoft':
- command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
- provider => 'shell'
+  command  => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
+  provider => 'shell'
 }
