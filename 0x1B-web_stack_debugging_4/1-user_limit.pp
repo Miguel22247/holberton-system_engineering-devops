4013845e78def0d80af2555a9b214592a41d79c2
Miguel Pacheco <minipachru@gmail.com>
2021-10-07 06:52:54 -0300

    Fix of puppet linter

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -3,6 +3,7 @@ exec { 'fixithard':
  command => 'sed -i "s/holberton hard nofile 5/holberton hard nofile 55000/g" /etc/security/limits.conf',
  provider => 'shell'
 }
+
 exec { 'fixitsoft':
  command => 'sed -i "s/holberton soft nofile 4/holberton soft nofile 55000/g" /etc/security/limits.conf',
  provider => 'shell'
